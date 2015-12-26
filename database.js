9a5a3e679fd119195dc956b200b20b7b7df05a2c
mehmet <mehmetarikan.ma@gmail.com>
2015-12-26 22:46:23 +0200

    start

---
+++ database.js
@@ -0,0 +1,228 @@
+var catalog = [
+    {
+        "itemName": 'Arduino',
+        "itemAuthor": 'Coşkun Taşdemir',
+        "itemimage": 'image/image1.jpg',
+        "itemDescription": '<p>İleri seviyede elektronik ve yazılım bilgisine sahip olmayan kişilerin de kolaylıkla interaktif projeler oluşturmasını sağlamak amacıyla tasarlanmış olan Arduino bugün dünyanın en popüler açık kaynak donanım projelerinden birisi haline gelmiş durumdadır. Bu kitapla Arduino ile tanışarak interaktif elektronik projeleri nasıl gerçekleştirebileceğinizi öğreneceksiniz. Yapabileceklerinizin hayal gücünüzle sınırlı olduğu Arduino dünyasına hoşgeldiniz!</p>',
+        "itemCreationDate": '2015',
+        "itemLanguage": 'türkçe',
+        "itemPublisher": 'Dikeyeksen',
+        "itemNumberofpages": '288'
+    },
+    
+    {
+        "itemName": 'C ile Bilgisaya Programlamaya Giriş',
+        "itemAuthor": 'Ali Orhan Aydın',
+        "itemimage": 'image/image2.jpg',
+        "itemDescription": '<p>Kendisini sürekli yenileyen ve günümüzde de yaygın olarak kullanılan C programlama dilini bu kitapla kolayca öğrenebileceksiniz. Programlamaya yeni başlayanlar için C dilini anlaşılır ve sade bir dille anlatan bu kitap sayesinde, temel kavramları öğrenebilecek ve örnek algoritmalar sayesinde öğrendiklerinizi pekiştirebileceksiniz. Bilgisayar programlamaya başlamak isteyen ama çok karmaşık olduğunu düşünüp, başlamaktan vazgeçen ve yeni başlayanlar için tam bir kaynak kitap.</p>',
+        "itemCreationDate": '2014',
+        "itemLanguage": 'türkçe',
+        "itemPublisher": 'Pusula Yayıncılık',
+        "itemNumberofpages": '400'
+    },
+    
+    {
+        "itemName": 'C# Programlama Dili ve Yazılım Tasarım',
+        "itemAuthor": 'Ahmet Kaymaz',
+        "itemimage": 'image/image3.jpg',
+        "itemDescription": '<p>Bu kitap konuları yaklaşımıyla üniversitelerin Bilgisayar Mühendisliği, Matematik-Bilgisayar, Yönetim Bilişim Sistemleri, Endüstri Mühendisliği, Yazılım Mühendisliği gibi mühendislik dört yıllık lisans ve meslek yüksek okulu öğrencileri için de yararlı kaynak özelliğindedir.</p>',
+        "itemCreationDate": '2008',
+        "itemLanguage": 'türkçe',
+        "itemPublisher": 'Papatya Yayıncılık',
+        "itemNumberofpages": '480'
+    },
+    
+    {
+        "itemName": "Çocuklar İçin Web Tasarımı",
+        "itemAuthor": 'Selçuk Özdemir',
+        "itemimage": "image/image4.jpg",
+        "itemDescription": '<p>Türkiye Bilişim Derneği, 40 yılı aşkın süredir yenilikçi bilişim teknolojileri üreten ve bunu toplumun yaşam kalitesini artırmak amacıyla kullanan nitelikli insan gücünün yetiştirilmesine büyük önem vermektedir. Bu bağlamda, Doç. Dr. Selçuk Özdeminin başlattığı "erken yaşta üretim odaklı bilişim teknolojisi eğitimi" çalışmalarının ülkemizin geleceği olan çocuk ve gençlerimizin bilişim teknolojilerinden doğru bir şekilde faydalanmaları yönünde farkındalık yaratacağını ve gerçek anlamda bir bilişim ve bilgi çağı toplumu olmamıza katkı sağlayacağını düşünüyorum.</p>',
+        "itemCreationDate": '2013',
+        "itemLanguage": 'türkçe',
+        "itemPublisher": 'Odtü',
+        "itemNumberofpages": '132'
+    },
+    
+    {
+        "itemName": "Her Yönüyle C# 6.0",
+        "itemAuthor": 'Volkan Aktaş',
+        "itemimage": 'image/image5.jpg',
+        "itemDescription": '<p>Elinizdeki kitapta, C# .NET in son sürümü olan 6.0 sürümü Visual Studio 2014 programı kullanılarak yenilikleri ile birlikte anlatılmıştır. C#.NET in temel özelliklerinden başlanarak orta seviyelere kadar birçok özellik uygulamalarla işlenmiştir. Bu özellikler içerisinde C# .NET in daha önceki sürümlerinden gelen özellikler olduğu gibi, 6.0 sürümü ile gelen yenilikler de yer almaktadır.</p>',
+        "itemCreationDate": '2015',
+        "itemLanguage": 'türkçe',
+        "itemPublisher": 'Kodlab',
+        "itemNumberofpages": '944'
+    },
+    
+    {
+        "itemName": "HTML 5 CSS 3",
+        "itemAuthor": 'Ahmet Oğuz Mermerkaya',
+        "itemimage": 'image/image6.jpg',
+        "itemDescription": '<p>Merhaba Android kitabının yazarlarından Ahmet Oğuz Mermerkaya, web tasarımı ve mobil geliştirme yapanların olmazsa olmazı olan HTML5 & CSS3 hakkında son güncellemeleri içeren yeni bir kitaba imza atıyor. Ahmet Oğuz Mermerkaya, teknik konuları, detaylarına inerek, konu hakkındaki temel kavramları okura benimseterek anlatmaya çalışan bir yazar. Akıcı bir dille kaleme alınan kitapta, HTML5 in ilginç özellik ve yenilikleri örneklerle açıklanıyor. Bu kitapta HTML5 in ne olduğu ve HTML5 ile gelen yenilikler hakkında örnekleriyle bilgiler bulacaksınız.</p>',
+        "itemCreationDate": '2015',
+        "itemLanguage": 'türkçe',
+        "itemPublisher": 'Abaküs Kitap',
+        "itemNumberofpages": '252'
+    },
+    
+    {
+        "itemName": 'Java ME',
+        "itemAuthor": 'Murat Balkan',
+        "itemimage": 'image/image7.jpg',
+        "itemDescription": '<p>Murat Balkan ın kaleme aldığı Java ME kitabı, Java Platformları ailesinde önemli bir yer tutan Java Micro Edition ı (Java ME) tüm detaylarıyla ele alıyor. Tamamen taşınabilir olan Java ME (eski ismiyle J2ME) ile yazılan bir uygulama, binlerce farklı tipte cihaz üzerinde sorunsuz olarak çalışmakta, bu da yazılım dağıtım sürecini ciddi oranda hızlandırmaktadır.</p>',
+        "itemCreationDate": '2015',
+        "itemLanguage": 'türkçe',
+        "itemPublisher": 'Pusula Yayıncılık',
+        "itemNumberofpages": '372'
+    },
+    
+     {
+        "itemName": 'Java Veri Yapıları',
+        "itemAuthor": 'Timur Karaçay',
+        "itemimage": 'image/image8.jpg',
+        "itemDescription": '<p>Bu kitap, Java Collections Framework çatısı içinde yer alan veri yapılarını ve o yapılar üzerinde işlem yapan metotları en kolay ve en hızlı biçimde ögretmek amacıyla yazılmıştır. Asıl hedef, JCF çatısının belirledigi hedeftir.</p>',
+        "itemCreationDate": '2011',
+        "itemLanguage": 'türkçe',
+        "itemPublisher": 'Pusula Yayıncılık',
+        "itemNumberofpages": '672'
+    },
+    
+     {
+        "itemName": 'Jquery',
+        "itemAuthor": 'Salih Baltalı',
+        "itemimage": 'image/image9.jpg',
+        "itemDescription": '<p>Bu kitapta akıcı, anlaşılır bir dil kullanmaya çalışılmıştır. Konuyu dağıtmadan meseleyi, az ve öz anlatılmıştır. Kitap içerisinde 3 ana bölüm bulunmaktadır. 600’den fazla ufak örnek, 50 adet uygulamalı örnek sunulmuştur. jQuery kütüphanesinde JavaScript komutları oldukça fazla ve ayrıca programlama mantığını da bilmek gerektiğinden JavaScript konusunu 2. bölümde anlatılmıştır.</p>',
+        "itemCreationDate": '2015',
+        "itemLanguage": 'türkçe',
+        "itemPublisher": 'Kodlab',
+        "itemNumberofpages": '410'
+    },
+    
+    {
+        "itemName": 'Kali Linux',
+        "itemAuthor": 'Abdulaziz Altuntaş',
+        "itemimage": 'image/image10.jpg',
+        "itemDescription": '<p>Kali Linux, bilişim güvenliği alanında çalışmalar yapan kişiler için oldukça kullanışlı, sağlam ve güvenli bir alt yapı ile gelmiştir. Kitabımızın içerisinde Kali Linux ile neler yapılabileceği anlatılmıştır. Bu kitapla bunların farkına varıp, uygulamalı bir şekilde öğrenebilirsiniz. Çoğu zaman bir senaryo üzerine kurulu olan içerik sayesinde, aradığınızı bulmakta zorlanmayacak ve bilişim güvenliği alanına yeni başlayanlariçin de bir hayal gücü ve başucu kaynağı olacaktır.</p>',
+        "itemCreationDate": '2015',
+        "itemLanguage": 'türkçe',
+        "itemPublisher": 'Kodlab',
+        "itemNumberofpages": '352'
+    },
+    
+    {
+        "itemName": 'Kim Korkar Java dan',
+        "itemAuthor": 'Alper Akalın',
+        "itemimage": 'image/image11.jpg',
+        "itemDescription": '<p>Java öğrenmeyi kafasına koyup yola çıkanlar artık korkup vazgeçmeyecek. NetBeans ortamında örneklerin herbir satırının neden yazıldığı anlatılarak Java ya giden yolu iyice aydınlatan Kim Korkar Java dan bize Open Source dünyasının kapılarını sonuna kadar açacak. Alper AKALIN ın anlatımıyla ve kod takibinin kolaylaştırdığı çalışmalarla hazırlanan Kim Korkar Java dan sizi bir Java programcısına dönüştürmek için hazır.</p>',
+        "itemCreationDate": '2014',
+        "itemLanguage": 'türkçe',
+        "itemPublisher": 'Pusula Yayıncılık',
+        "itemNumberofpages": '536'
+    },
+    
+    {
+        "itemName": 'Linux',
+        "itemAuthor": 'Prof. Dr. Mithat Uysal',
+        "itemimage": 'image/image12.jpg',
+        "itemDescription": '<p>Bilgisayar ile ilişkili bir eğitim görüyorsanız, çok kullanıcılı işletim sistemleri içinde kararlılık ve güvenilirlik bakımından en popüleri olan UNIX işletim sistemini öğrenmek zorundasınız demektir. Ancak UNIX, PC ler üzerine yüklenemez. Evde kullanabileceğiniz bir PC üzerine ancak Linux u yükleyebilirsiniz ve Linux, komut ve özellikleri bakımından, grafik arayüzleri bakımından UNIX in tam bir kopyasıdır. O halde evdeki UNIX laboratuvarınız, bir Linux makinasıdır.</p>',
+        "itemCreationDate": '2014',
+        "itemLanguage": 'türkçe',
+        "itemPublisher": 'Nirvana Yayınları',
+        "itemNumberofpages": '386'
+    },
+    
+      {
+        "itemName": 'Örneklerle Pratik Java',
+        "itemAuthor": 'Mustafa Dikici',
+        "itemimage": 'image/image13.jpg',
+        "itemDescription": '<p>Java, bünyesinde birçok platform barındırmaktadır. Mobil uygulamalar, masaüstü uygulamaları, kurumsal uygulamalar ve web tabanlı uygulamalar için farklı platformları bulunur. Hangi platformda çalışırsanız çalışın, temel Java bilgisine yani JavaSE bilgisine sahip olmanız gerekir. Bu kitapta da JavaSE ele alınmış ve temel Java anlatılmıştır.</p>',
+        "itemCreationDate": '2013',
+        "itemLanguage": 'türkçe',
+        "itemPublisher": 'Seçkin Yayıncılık',
+        "itemNumberofpages": '392'
+    },
+    
+    
+      {
+        "itemName": 'Python',
+        "itemAuthor": 'Mustafa Başer',
+        "itemimage": 'image/image14.jpg',
+        "itemDescription": '<p>Python, ilk sürümünden (1990 yılının başında) bu yana dikkatleri üzerine çeken genel amaçlı, yorumlamalı, nesne tabanlı, temiz kod yazılabilen, hızlı öğrenilebilen, hızlı uygulama geliştirilebilen ve birçok aracı (kütüphaneyi) içeren bir programlama dilidir. Bu kitap, programcılığa yeni başlayanlar için bir rehber, Python u bilenler için bir başvuru kaynağı niteliğindedir.</p>',
+        "itemCreationDate": '2015',
+        "itemLanguage": 'türkçe',
+        "itemPublisher": 'Dikeyeksen',
+        "itemNumberofpages": '538'
+    },
+    
+    
+    {
+        "itemName": 'Yeni Başlayanlar İçin Java',
+        "itemAuthor": 'Mehmet Kirazlı',
+        "itemimage": 'image/image15.jpg',
+        "itemDescription": '<p>Java programlama dili; platformdan bağımsız olarak çalışması, nesneye yönelik olması, güvenliği ön planda tutması, esnek olması ve daha bunun gibi birçok artıları ile tercih edilen bir dil olmuştur ve olmaya da devam edecektir. Bu yüzdendir ki Java yı geliştiren Sun Microsystems firmasını satın alan Oracle, Java nın bu üstünlüklerini keşfetmiş ve Java yı bünyesine katmıştır. Java nın en önemli faydası ise her işletim sisteminde çalışmasıdır.</p>',
+        "itemCreationDate": '2015',
+        "itemLanguage": 'türkçe',
+        "itemPublisher": 'Kodlab',
+        "itemNumberofpages": '656'
+    },
+    
+    {
+        "itemName": 'Beyond Java',
+        "itemAuthor": 'Bruce Tate',
+        "itemimage": 'image/image16.jpg',
+        "itemDescription": '<p>Bruce Tate chronicles the rise of the most successful language of all time, and then lays out, in painstaking detail, the compromises the founders had to make to establish success. Then, he describes the characteristics of likely successors to Java.</p>',
+        "itemCreationDate": '2005',
+        "itemLanguage": 'ingilizce',
+        "itemPublisher": 'O.Reilly Media, Inc.',
+        "itemNumberofpages": '200'
+    },
+    
+    
+     {
+        "itemName": 'Java Look and Feel Design Guidelines',
+        "itemAuthor": 'Sun Microsystems Inc.',
+        "itemimage": 'image/image17.jpg',
+        "itemDescription": '<p>Java Look and Feel Design Guidelines, Second Edition, provides essential information for anyone involved in creating cross-platform GUI (graphical user interface) applications and applets in the Java programming language. In particular, this book offers design guidelines for software that uses the Swing classes together with the</p>',
+        "itemCreationDate": '2001',
+        "itemLanguage": 'ingilizce',
+        "itemPublisher": 'Addison-Wesley Professional',
+        "itemNumberofpages": '416'
+    },
+    
+    
+    {
+        "itemName": 'Programming Computer Vision with Python',
+        "itemAuthor": 'Jan Erik Solem',
+        "itemimage": 'image/image18.jpg',
+        "itemDescription": '<p>The idea behind this book is to give an easily accessible entry point to hands-on computer vision with enough understanding of the underlying theory and algorithms to be a foundation for students, researchers and enthusiasts.</p>',
+        "itemCreationDate": '2012',
+        "itemLanguage": 'ingilizce',
+        "itemPublisher": 'O.Reilly Media',
+        "itemNumberofpages": '300'
+    },
+    
+    {
+        "itemName": 'Python Cookbook',
+        "itemAuthor": 'David Beazley, Brian K. Jones',
+        "itemimage": 'image/image19.jpg',
+        "itemDescription": '<p>Packed with practical recipes written and tested with Python 3.3, this unique cookbook is for experienced Python programmers who want to focus on modern tools and idioms. Inside, you ll find complete recipes for more than a dozen topics, covering the core Python language as well as tasks common to a wide variety of application domains.</p>',
+        "itemCreationDate": '2013',
+        "itemLanguage": 'ingilizce',
+        "itemPublisher": 'O.Reilly Media',
+        "itemNumberofpages": '708'
+    },
+    
+    {
+        "itemName": 'C++ The Complete Reference',
+        "itemAuthor": 'Herbert Schildt',
+        "itemimage": 'image/image20.jpg',
+        "itemDescription": '<p>Master programmer and best-selling author Herb Schildt has updated and expanded his classic reference to C++. Using expertly crafted explanations, insider tips, and hundreds of examples, Schildt explains and demonstrates every aspect of C++. Inside you ll find details on the entire C++ language, including its keywords, operators, preprocessor directives, and libraries. There is even a synopsis of the extended keywords used for .NET programming. Of course, everything is presented in the clear, crisp, uncompromising style that has made Herb Schildt the choice of millions.</p>',
+        "itemCreationDate": '2002',
+        "itemLanguage": 'ingilizce',
+        "itemPublisher": 'McGraw-Hill Education',
+        "itemNumberofpages": '1056'
+    }
+    
+    
+    
+];
